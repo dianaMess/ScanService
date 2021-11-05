e162ec0ee2633ce1edf4384d527fe60dd5e29d5a
Диана Оспанова <ospanovadiana2000@outlook.com>
2021-11-05 21:32:01 +0300

    Добавьте файлы проекта.

---
+++ ScanService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ScanService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strr = "\n";
+            do
+            {
+                strr = Console.ReadLine();
+                string[] strs = strr.Split(' ');
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.UseShellExecute = false;
+                    string path = Environment.CurrentDirectory;
+                    string[] s = path.Split(@"\");
+                    string add = s[^1];
+                    string full = @"../../../..\scan_util\bin\Debug\" + add + @"\scan_util";
+                    myProcess.StartInfo.FileName = full;
+                    Console.WriteLine(full);
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.StartInfo.Arguments = strs[2];
+                    myProcess.StartInfo.RedirectStandardOutput = true;
+                    myProcess.Start();
+                    Console.WriteLine(myProcess.StandardOutput.ReadToEnd());
+                    myProcess.WaitForExit();
+                }
+            } while (strr != "\n");
+        }
+    }
+}
---
+++ scan_util/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+namespace scan_util
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)   //check if path wasn't given in command line
+            {
+                Console.WriteLine("Path is not given");
+            }
+            else
+            {
+                string path = args[0];
+                int counter_1 = 0, counter_2 = 0, counter_3 = 0, files_counter = 0, errors = 0;
+                string sample1 = "<script>evil_script()</script>";
+                string sample2 = "rm -rf " + path;
+                string sample3 = "Rundll32 sus.dll SusEntry";
+                var watch = Stopwatch.StartNew();
+                if (Directory.Exists(path))
+                {
+                    string[] fileEntries = Directory.GetFiles(path);  //get all files from given directory
+                    foreach (string fileName in fileEntries)
+                    {
+                        files_counter++;
+                        try
+                        {
+                            if (File.Exists(fileName))
+                            {
+                                string text = File.ReadAllText(fileName);
+                                string extension = Path.GetExtension(fileName);
+                                if (extension == ".js")   //check is file a js file
+                                {
+                                    if (text.Contains(sample1))   //check if file contains a string "<script>exile_script()</script>"
+                                    {
+                                        counter_1++;
+                                    }
+                                }
+                                else if (text.Contains(sample2))  //check if file contains a string "rm -rf %directory_path%"
+                                {
+                                    counter_2++;
+                                }
+                                else if (text.Contains(sample3))  //check if file contains a string "Rundll32 sus.dll SusEntry"
+                                {
+                                    counter_3++;
+                                }
+                            }
+                        }
+                        catch (Exception)          //catch all errors while analyzing files
+                        {
+                            errors++;
+                        }
+                    }
+                    watch.Stop();
+                    var elapsed = watch.Elapsed;
+                    //print results
+                    //Console.WriteLine(Environment.CurrentDirectory);
+                    Console.WriteLine("====== Scan result ======");
+                    Console.WriteLine("Processed files: " + files_counter);
+                    Console.WriteLine("JS detects: " + counter_1);
+                    Console.WriteLine("rm -rf detects: " + counter_2);
+                    Console.WriteLine("Rundll32 detects: " + counter_3);
+                    Console.WriteLine("Errors: " + errors);
+                    Console.WriteLine("Execution time: {0:00}:{1:00}:{2:00}", elapsed.Minutes, elapsed.Seconds, elapsed.Milliseconds);
+                    Console.WriteLine("=========================");
+                }
+                else
+                {
+                    Console.WriteLine("Path doesn't exist");
+                }
+            }
+        }
+    }
+}
